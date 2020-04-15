80d0497f5c2bd5a23286718564e1800f0d960ba9
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-04-15 16:41:46 +0530

    Use that BLoC , here increment is performed but we can not see any changes

--- bin/main.dart
+++ bin/main.dart
@@ -1,7 +1,13 @@
-
+import './counter_bloc.dart';
 
 
 
 void main(List<String> arguments) {
+
+  CounterBloc bloc = CounterBloc();
+
+    for (int i = 0; i < 3; i++) {
+        bloc.add(CounterEvent.increment);
+    }
   
 }
