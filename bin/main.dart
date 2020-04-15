1157925a5825a011ba143765ed42fc6ee90e17a9
Bhavik Vashi <bhavikvashi1804@gmail.com>
2020-04-15 16:55:21 +0530

    Consume the BLoCDelegate which prints Transitions + here our onTransition also prints all Transitions

--- bin/main.dart
+++ bin/main.dart
@@ -1,10 +1,14 @@
+import 'package:bloc/bloc.dart';
+
 import './counter_bloc.dart';
+import './bloc_delegate.dart';
 
 
 
 void main(List<String> arguments) {
 
   CounterBloc bloc = CounterBloc();
+  BlocSupervisor.delegate = SimpleBlocDelegate();
 
     for (int i = 0; i < 3; i++) {
         bloc.add(CounterEvent.increment);
